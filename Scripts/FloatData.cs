ec21daebbf7a8bf8156f48556fac7f61cbc0c986
Sierra Weber <10889154>
2021-11-03 17:26:41 -0600

    Scripts

---
+++ Scripts/FloatData.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[ExecuteInEditMode, CreateAssetMenu(menuName = "SingleVariables/FloatData")]
+public class FloatData : NameID
+{
+    public float value;
+    public UnityEvent minValueEvent, maxValueEvent, updateValueEvent;
+
+    public void SetValue(float amount)
+    {
+        value += amount;
+        updateValueEvent.Invoke();
+    }
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+        updateValueEvent.Invoke();
+    }
+
+    public void IncrementValue()
+    {
+        value++;
+    }
+
+    public void UpdateValue(FloatData data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) value += newData.value;
+    }
+
+    public void SetValue(FloatData data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) value += newData.value;
+    }
+
+    public void CheckMinValue(float minValue)
+    {
+        if (!(value < minValue)) return;
+        minValueEvent.Invoke();
+        value = minValue;
+    }
+
+    public void CheckMaxValue(float maxValue)
+    {
+        if (!(value >= maxValue)) return;
+        maxValueEvent.Invoke();
+        value = maxValue;
+    }
+}
